64f4d8b8e7893c4f4926e749ba561c5775b3e1ad
LinCie <aldiofernanda@protonmail.com>
2026-01-19 00:22:59 +0700

    fix(utility): handle various types in safeBigintToNumber

--- src/utilities/transform.utility.ts
+++ src/utilities/transform.utility.ts
@@ -1,10 +1,11 @@
-function safeBigintToNumber(x: bigint): number {
+function safeBigintToNumber(x: string | number | bigint): number {
+  const value = BigInt(x);
   const max = BigInt(Number.MAX_SAFE_INTEGER);
   const min = BigInt(Number.MIN_SAFE_INTEGER);
-  if (x > max || x < min) {
+  if (value > max || value < min) {
     throw new RangeError("bigint out of safe number range");
   }
-  return Number(x);
+  return Number(value);
 }
 
 export { safeBigintToNumber };
